4086a151cb993049e05ee32295ebb6aa6b424120
leeminh888 <68751029+leeminh888@users.noreply.github.com>
2021-04-16 15:44:43 +0700

    Add files via upload

---
+++ AnyKernel3/anykernel.sh
@@ -0,0 +1,39 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=LithoWonder-EAS
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=X01BD
+device.name2=X01BDA
+device.name3=
+device.name4=
+device.name5=
+supported.versions=10  - 11
+supported.patchlevels=2017-07 -
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel install
+dump_boot;
+
+# end ramdisk changes
+
+write_boot;
+## end install
+
